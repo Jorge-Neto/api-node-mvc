ba29845e1cde1fd3a8b9b3e62ec5ee603dd0da8a
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-04-29 10:01:01 -0300

    First commit - Project Base

---
+++ src/app/controllers/customersController.js
@@ -0,0 +1,66 @@
+let customers = [
+    { id: 1, name: "Jorge", language: "JavaScript" },
+    { id: 2, name: "Oliveira", language: "Java" },
+    { id: 3, name: "Neto", language: "C++" }
+];
+
+class CustomersController {
+
+    // Lista todos os Customers
+    index(req, res) {
+        return res.json(customers);
+    }
+
+    // Lista um Customer
+    show(req, res) {
+        const customerId = parseInt(req.params.id);
+
+        const customer = customers.find(item => item.id === customerId);
+        const status = customer ? 200 : 404;
+
+        return res.status(status).json(customer);
+    }
+
+    // Cria um Customer
+    create(req, res) {
+        const { name, language } = req.body;
+        const id = customers[customers.length - 1].id + 1;
+
+        const newCustomer = { id, name, language };
+        customers.push(newCustomer);
+
+        return res.status(201).json(newCustomer);
+    }
+
+    // Atualiza um Customer
+    update(req, res) {
+        const id = parseInt(req.params.id);
+        const { name, language } = req.body;
+
+        const index = customers.findIndex(item => item.id === id);
+        const status = index >= 0 ? 200 : 404;
+
+        if (index >= 0) {
+            customers[index] = { id: id, name, language };
+        }
+
+        return res.status(status).json(customers[index]);
+    }
+
+    // Exclui um Customer
+    destroy(req, res) {
+        const id = parseInt(req.params.id);
+
+        const index = customers.findIndex(item => item.id === id);
+        const status = index >= 0 ? 200 : 404;
+
+        if (index >= 0) {
+            customers.splice(index, 1);
+        }
+
+        return res.status(status).json(customers);
+    }
+
+}
+
+module.exports = new CustomersController();
