529e690e94fca5288d0a25e2d0f7fad5dc970497
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-05-04 17:30:53 -0300

    Implementando a CustomerController

--- src/app/controllers/customersController.js
+++ src/app/controllers/customersController.js
@@ -1,3 +1,6 @@
+import { Op } from "sequelize";
+import { parseISO } from "date-fns";
+
 import Customer from "../models/Customer";
 
 const customers = [
@@ -9,8 +12,92 @@ const customers = [
 class CustomersController {
   // List All Customers
   async index(req, res) {
+    const {
+      name,
+      email,
+      status,
+      createdBefore,
+      createdAfter,
+      updatedBefore,
+      updatedAfter,
+      sort,
+    } = req.query;
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 25;
+
+    let where = {};
+    let order = [];
+
+    if (name) {
+      where = {
+        ...where,
+        name: {
+          [Op.iLike]: name,
+        },
+      };
+    }
+
+    if (email) {
+      where = {
+        ...where,
+        email: {
+          [Op.iLike]: email,
+        },
+      };
+    }
+
+    if (status) {
+      where = {
+        ...where,
+        status: {
+          [Op.in]: status.split(",").map((item) => item.toUpperCase()),
+        },
+      };
+    }
+
+    if (createdBefore) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.gte]: parseISO(createdBefore),
+        },
+      };
+    }
+    if (createdAfter) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.lte]: parseISO(createdAfter),
+        },
+      };
+    }
+    if (updatedBefore) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.gte]: parseISO(updatedBefore),
+        },
+      };
+    }
+    if (updatedAfter) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.lte]: parseISO(updatedAfter),
+        },
+      };
+    }
+
+    if (sort) {
+      order = sort.split(",").map((item) => item.split(":"));
+    }
+
     const data = await Customer.findAll({
-      limit: 1000,
+      where,
+      order,
+      limit,
+      offset: limit * page - limit,
     });
     return res.json(data);
   }
